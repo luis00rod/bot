aaa9a0db59576708c8bbba697956f2cc76963661
imunky <victorvrodriguez@gmail.com>
2023-04-16 17:27:54 +0200

    Refactor begging. Some module implemented

--- src/main/common/connection.py
+++ src/main/common/connection.py
@@ -6,17 +6,20 @@ import numpy as np
 class Connection(object):
 
     def __init__(self):
-        self.apy_key = "WSbzvk0MmvVCMeHLYWhyepHg6"
-        self.apy_key_secret = "7FeUI4FKeA0gtYANDg4WHw5AodQXYTbZ88YydLc9pfnrBx4LgZ"
-        self.apy_key_openai = "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"
-        self.consumer_key = 'WSbzvk0MmvVCMeHLYWhyepHg6'
-        self.consumer_secret = '7FeUI4FKeA0gtYANDg4WHw5AodQXYTbZ88YydLc9pfnrBx4LgZ'
-        self.access_token = '592854692-VSja6WOJEo9Idi9JVP5xenuehwGt7QFDDaV4ZOjW'
-        self.access_secret = 'wodlFzbEjiJ40ZPvVoV4UwUXjDoDf1lLUOcOjZ7ynuHPr'
-        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        self.auth.set_access_token(self.access_token, self.access_secret)
+
         pass
 
-    def connect_to(self, auth):
+    def twitter_conn(self):
+        apy_key = "WSbzvk0MmvVCMeHLYWhyepHg6"
+        apy_key_secret = "7FeUI4FKeA0gtYANDg4WHw5AodQXYTbZ88YydLc9pfnrBx4LgZ"
+        apy_key_openai = "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"
+        consumer_key = 'WSbzvk0MmvVCMeHLYWhyepHg6'
+        consumer_secret = '7FeUI4FKeA0gtYANDg4WHw5AodQXYTbZ88YydLc9pfnrBx4LgZ'
+        access_token = '592854692-VSja6WOJEo9Idi9JVP5xenuehwGt7QFDDaV4ZOjW'
+        access_secret = 'wodlFzbEjiJ40ZPvVoV4UwUXjDoDf1lLUOcOjZ7ynuHPr'
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_secret)
         return tweepy.API(auth)
 
+    def get_news(self):
+        pass
